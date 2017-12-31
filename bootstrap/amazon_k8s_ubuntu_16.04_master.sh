8bf7466b3d7044b92e1831d6b1bc3864115c37ab
Eric Charles <eric@datalayer.io>
2017-12-31 15:23:40 +0100

    shell format

--- bootstrap/amazon_k8s_ubuntu_16.04_master.sh
+++ bootstrap/amazon_k8s_ubuntu_16.04_master.sh
@@ -114,7 +114,8 @@ function install_helm() {
   kubectl create serviceaccount --namespace kube-system tiller
   kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
   kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'      
-  helm init --service-account tiller --upgrade; kubectl rollout status -w deployment/tiller-deploy --namespace=kube-system;
+  helm init --service-account tiller --upgrade
+  kubectl rollout status -w deployment/tiller-deploy --namespace=kube-system
 }
 
 setup_rbac
